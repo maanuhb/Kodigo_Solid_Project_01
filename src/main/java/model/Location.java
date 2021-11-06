6d4335dcb1896776b83dd20df7dc2e7bb2e81a83
SpaceDev42 <vladyguevara6@gmail.com>
2021-11-06 10:12:55 -0600

    refactoring Airport class

--- src/main/java/model/Location.java
+++ src/main/java/model/Location.java
@@ -1,8 +1,18 @@
 package model;
 
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Location {
+   @NonNull
    private String CountryName;
+   @NonNull
+   private String CountryCode;
    private String cityName;
    private String cityAbbreviation;
 
